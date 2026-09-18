dfba8370687e4c5bc86796c58d2e987a8281506f
jack <45120567+jaccqo@users.noreply.github.com>
2026-09-18 00:42:38 -0400

    Render the reference home template

--- config/views.py
+++ config/views.py
@@ -1,12 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse(
-        """
-        <main style="font-family: sans-serif; max-width: 720px; margin: 80px auto;">
-            <h1>Authkits Django Test App</h1>
-            <p>Authkits is installed and the Django integration is running.</p>
-        </main>
-        """
-    )
+    return render(request, "home.html")
